b6a3fafb18968065ef22a008d54831dfcf52ff03
Matsuura Miu <sinikake0519@yahoo.co.jp>
2024-03-26 00:16:44 +0900

    不要なインポートの削除 少しコメント追加

--- app/src/main/java/com/websarva/wings/android/gohandoko/getLocationInfo/LocationInfoActivity.kt
+++ app/src/main/java/com/websarva/wings/android/gohandoko/getLocationInfo/LocationInfoActivity.kt
@@ -2,36 +2,28 @@ package com.websarva.wings.android.gohandoko.getLocationInfo
 
 import android.location.Location
 import android.location.LocationListener
-import android.location.LocationManager
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.websarva.wings.android.gohandoko.ui.theme.GoHandokoTheme
 
-class LocationInfoActivity:ComponentActivity(),LocationListener {
+class LocationInfoActivity : ComponentActivity(), LocationListener {
     private lateinit var locationInfomation: LocationInfomation
 
-    override fun onCreate(savedInstanceState:Bundle?){
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-         locationInfomation= LocationInfomation(this,this)
-         locationInfomation.getLocation()
+        locationInfomation = LocationInfomation(this, this)
+        locationInfomation.getLocation()
     }
 
     override fun onLocationChanged(location: Location) {
         Log.d("LocationInfoDebug", "経度：${location.longitude}")
         Log.d("LocationInfoDebug", "緯度：${location.latitude}")
-        setContent{
-            locationScreen(location.longitude,location.latitude)
+        setContent {
+            locationScreen(location.longitude, location.latitude)
         }
     }
 
@@ -40,8 +32,8 @@ class LocationInfoActivity:ComponentActivity(),LocationListener {
 @Composable
 fun locationScreen(latitude: Double = 0.0, longitude: Double = 0.0) {
     Column {
-        Text(text ="経度：$longitude")
-        Text(text="緯度：$latitude")
+        Text(text = "経度：$longitude")
+        Text(text = "緯度：$latitude")
 
     }
 }
--- app/src/main/java/com/websarva/wings/android/gohandoko/getLocationInfo/LocationInfomation.kt
+++ app/src/main/java/com/websarva/wings/android/gohandoko/getLocationInfo/LocationInfomation.kt
@@ -3,30 +3,21 @@ package com.websarva.wings.android.gohandoko.getLocationInfo
 
 import android.Manifest
 import android.content.Context
-import android.content.Intent
 import android.content.pm.PackageManager
-import android.location.Location
 import android.location.LocationListener
-import android.location.LocationManager
-import android.os.Bundle
-import android.provider.Settings
-import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import com.websarva.wings.android.gohandoko.ui.theme.GoHandokoTheme
 
 
-class LocationInfomation(private val activity: ComponentActivity,private  val locationListener: LocationListener) {
+class LocationInfomation(
+    private val activity: ComponentActivity,
+    private val locationListener: LocationListener
+) {
     /** LocationManagerを保持する変数 **/
-    private val locationManager=activity.getSystemService(Context.LOCATION_SERVICE)as android.location.LocationManager
+    private val locationManager =
+        activity.getSystemService(Context.LOCATION_SERVICE) as android.location.LocationManager
 
     /**
      * 位置情報の許可を求める
@@ -38,7 +29,8 @@ class LocationInfomation(private val activity: ComponentActivity,private  val lo
             if (isGranted) {
                 getLocation()
             } else {
-                val toast = Toast.makeText(activity, "位置情報許可してくれないと検索できないよ！", Toast.LENGTH_LONG)
+                val toast =
+                    Toast.makeText(activity, "位置情報許可してくれないと検索できないよ！", Toast.LENGTH_LONG)
                 toast.show()
             }
         }
@@ -55,12 +47,17 @@ class LocationInfomation(private val activity: ComponentActivity,private  val lo
         ) {
             requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
         } else {
-                updateLocation()
+            updateLocation()
         }
 
     }
 
-    private fun updateLocation(){
+    /**
+     * 位置情報の更新
+     */
+    private fun updateLocation() {
+
+        //パーミッションが既に得られているかどうかを確認しないとエラーになる？
         if (ContextCompat.checkSelfPermission(
                 activity,
                 Manifest.permission.ACCESS_FINE_LOCATION
