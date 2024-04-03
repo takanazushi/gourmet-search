85592f5d46fb317c860ce966b592bbe000ea97e5
Matsuura Miu <sinikake0519@yahoo.co.jp>
2024-04-03 11:02:53 +0900

    targetSdkの変更

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     defaultConfig {
         applicationId = "com.websarva.wings.android.gohandoko"
         minSdk = 27
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
